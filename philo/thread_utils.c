e24dd5ba1dbd57fd7301bc1d6653c7c33d4a1efa
Nathan <nathan.s.ranna@gmail.com>
2024-07-10 11:12:19 -0300

    I did it

--- philo/thread_utils.c
+++ philo/thread_utils.c
@@ -6,7 +6,7 @@
 /*   By: nranna <nranna@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 11:29:23 by nranna            #+#    #+#             */
-/*   Updated: 2024/07/01 16:45:45 by nranna           ###   ########.fr       */
+/*   Updated: 2024/07/10 11:08:51 by nranna           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,16 +25,31 @@ void	give_threads(t_table *table, int philo_amount)
 	while (i < philo_amount)
 	{
 		pthread_create(&table->philo[i]->thread_id, NULL,
-				&ft_nothing, NULL);
-		pthread_join(table->philo[i]->thread_id, NULL);
+				&ft_nothing, &table->philo[i]);
 		i++;
 	}
 	i = 0;
+	while (i < philo_amount)
+	{
+		pthread_join(&table->philo[i]->thread_id, NULL);
+		i++;
+	}
 	return ;
 }
 
-void	*ft_nothing(void *ft)
+void *ft_nothing(void *ft)
 {
-	printf("I wont eat nor think... but I'll sleep!\n");
+	t_philo *philo = (t_philo *)ft;
+	printf("Thread %d: I wont eat nor think... but I'll sleep!\n", philo->philo_id);
+	while (1)
+		sleep(1);
 	return (NULL);
 }
+
+/*void	*ft_nothing(void *ft)
+{
+	printf("Thread %d: I wont eat nor think... but I'll sleep!\n", data->id);
+	while (1)
+		sleep(1);
+	return (NULL);
+}*/
